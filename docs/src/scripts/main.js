429f0c756058edaebd5a5c75836899c087faf8ea
pchincoa <pchincoa@gmail.com>
2021-08-07 01:48:39 +0200

    init

--- docs/src/scripts/main.js
+++ docs/src/scripts/main.js
@@ -1,13 +1,13 @@
 import LocomotiveScroll from '../../../src/locomotive-scroll';
 
-(function() {
+(function () {
 
     document.documentElement.classList.add('is-loaded');
     document.documentElement.classList.remove('is-loading');
 
     setTimeout(() => {
         document.documentElement.classList.add('is-ready');
-    },300)
+    }, 300)
 
     let options = {
         el: document.querySelector('#js-scroll'),
@@ -16,7 +16,7 @@ import LocomotiveScroll from '../../../src/locomotive-scroll';
         getDirection: true
     }
 
-    if(document.querySelector('#js-scroll').getAttribute('data-horizontal') == 'true') {
+    if (document.querySelector('#js-scroll').getAttribute('data-horizontal') == 'true') {
         options.direction = 'horizontal';
         options.gestureDirection = 'both';
         options.tablet = {
@@ -54,28 +54,28 @@ import LocomotiveScroll from '../../../src/locomotive-scroll';
 
         scroll.on('call', (value, way, obj) => {
             if (value === 'dynamicBackground') {
-                if(way === 'enter') {
+                if (way === 'enter') {
                     dynamicBackgrounds.push({
                         id: obj.id,
                         el: obj.el
                     });
                 } else {
                     for (var i = 0; i < dynamicBackgrounds.length; i++) {
-                        if(obj.id === dynamicBackgrounds[i].id) {
-                            dynamicBackgrounds.splice(i,1);
+                        if (obj.id === dynamicBackgrounds[i].id) {
+                            dynamicBackgrounds.splice(i, 1);
                         }
                     }
                 }
             } else if (value === 'dynamicColor') {
-                if(way === 'enter') {
+                if (way === 'enter') {
                     dynamicColorElements.push({
                         id: obj.id,
                         el: obj.el
                     });
                 } else {
                     for (var i = 0; i < dynamicColorElements.length; i++) {
-                        if(obj.id === dynamicColorElements[i].id) {
-                            dynamicColorElements.splice(i,1);
+                        if (obj.id === dynamicColorElements[i].id) {
+                            dynamicColorElements.splice(i, 1);
                         }
                     }
                 }
@@ -84,6 +84,7 @@ import LocomotiveScroll from '../../../src/locomotive-scroll';
 
     }, 1000)
 
+    // JS for Smartphone and Tablet
 
     var scroll = new LocomotiveScroll({
         el: document.querySelector('[data-scroll-container]'),
